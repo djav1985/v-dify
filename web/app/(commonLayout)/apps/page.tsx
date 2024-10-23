a60dd8cb58cb9df55374b02ae12b86d0de3a3a9d
Vontainment <djav1985@gmail.com>
2024-10-23 16:36:41 -0400

    Update page.tsx

--- web/app/(commonLayout)/apps/page.tsx
+++ web/app/(commonLayout)/apps/page.tsx
@@ -11,7 +11,7 @@ const AppList = async () => {
     <div className='relative flex flex-col overflow-y-auto bg-gray-100 shrink-0 h-0 grow'>
       <Apps />
       <footer className='px-12 py-6 grow-0 shrink-0'>
-        <h3 className='text-xl font-semibold leading-tight text-gradient'>VONTAINMENT</h3>
+        <h3 className='text-xl font-semibold leading-tight text-gradient'>Vontainment AI Platform</h3>
       </footer>
     </div >
   )
